4d465b1fcf508407f9235589e4dc13e3cfb2d3b0
961026bm <olubunmiosunde.student@careerdevs.com>
2022-01-17 22:28:04 -0500

    calcAge done

---
+++ src/Age.java
@@ -0,0 +1,9 @@
+public class Age {
+    public static void main(String[] args) {
+        System.out.println(calcAge(45));
+    }
+    public static int calcAge(int age) {
+        return age * 365;
+
+    }
+}
