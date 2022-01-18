25a80b6d732df39c5e0f024bba222459334ace7e
961026bm <olubunmiosunde.student@careerdevs.com>
2022-01-17 22:35:41 -0500

    HelloName done

---
+++ src/HelloName.java
@@ -0,0 +1,9 @@
+public class HelloName {
+
+    public static void main(String[] args) {
+        System.out.println(helloName("Barrack"));
+    }
+    public static String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+}
